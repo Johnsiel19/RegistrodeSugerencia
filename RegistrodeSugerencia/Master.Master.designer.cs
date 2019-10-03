d579f1bb135b472b3abd71f1a97f6abd1535537f
Johnsiel Antonio Castanos Hernandez <johnsiel_castanos@ucne.edu.do>
2019-10-02 22:19:09 -0400

    Listo

--- RegistrodeSugerencia/Master.Master.designer.cs
+++ RegistrodeSugerencia/Master.Master.designer.cs
@@ -19,7 +19,7 @@ namespace RegistrodeSugerencia {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.PlaceHolder head;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
         /// form1 control.
